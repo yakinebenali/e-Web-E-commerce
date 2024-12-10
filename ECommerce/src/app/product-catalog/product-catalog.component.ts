13eeccc66644f1586b1bf72eeaff85158dd4fdd0
yakine123 <yakinebenali5@gmail.com>
2024-12-10 21:32:14 +0100

    fix :`app html`

--- ECommerce/src/app/product-catalog/product-catalog.component.ts
+++ ECommerce/src/app/product-catalog/product-catalog.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../product.model';
-
-
-
 import { ProductService } from '../product.service';
-import{CartService}from'../cart.service';
+import { CartService } from '../cart.service';
+
 @Component({
   selector: 'app-product-catalog',
   templateUrl: './product-catalog.component.html',
@@ -12,11 +10,26 @@ import{CartService}from'../cart.service';
 })
 export class ProductCatalogComponent implements OnInit {
   products: Product[] = [];
-  
+  filteredProducts: Product[] = []; // Propriété pour les produits filtrés
+  searchQuery: string = ''; // Propriété pour la recherche
+
   constructor(private productService: ProductService, private cartService: CartService) {}
 
   ngOnInit(): void {
     this.products = this.productService.getProducts();
+    this.filteredProducts = this.products; // Initialisation de filteredProducts
+  }
+
+  // Méthode pour filtrer les produits en fonction de searchQuery
+  filterProducts(): void {
+    if (this.searchQuery) {
+      this.filteredProducts = this.products.filter(product =>
+        product.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        product.category.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    } else {
+      this.filteredProducts = this.products; // Si la recherche est vide, on affiche tous les produits
+    }
   }
 
   addToCart(product: Product): void {
